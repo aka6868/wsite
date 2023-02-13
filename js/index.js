8e38fdce0b588c03d8cb678972bf5620c71b69ec
Billy <contact@akatek.vn>
2023-02-13 15:43:37 +0700

    initial commit

--- js/index.js
+++ js/index.js
@@ -6,33 +6,26 @@
 // import {initSwiperSlider} from "./modules/slider";
 // import setYTFrame from "./modules/video";
 
-document.addEventListener('DOMContentLoaded', () => {
-    initSwiperSlider('.chart_slider', '.chart_slider', {
-        effect: "coverflow",
-        centeredSlides: true,
-        autoplay: true,
-        speed: 700,
-        slidesPerView: "auto",
-        spaceBetween: 30,
-        initialSlide: 2,
-        coverflowEffect: {
-            rotate: 0,
-            stretch: 0,
-            depth: 100,
-            modifier: 2,
-            slideShadows: false,
-        },
-        breakpoints: {
-            767.98: {
-                spaceBetween: 0
-            }
-        }
-    })
-});
-
-const navItems = document.querySelectorAll(".header_nav-list_item")
-const headerNav = document.querySelector("#headerMenu")
-navItems.forEach(e=>{e.addEventListener("click",e=>{
-    console.log("accccc");
-    headerNav.classList.remove("show");
-})});
+// document.addEventListener('DOMContentLoaded', () => {
+//     initSwiperSlider('.chart_slider', '.chart_slider', {
+//         effect: "coverflow",
+//         centeredSlides: true,
+//         autoplay: true,
+//         speed: 700,
+//         slidesPerView: "auto",
+//         spaceBetween: 30,
+//         initialSlide: 2,
+//         coverflowEffect: {
+//             rotate: 0,
+//             stretch: 0,
+//             depth: 100,
+//             modifier: 2,
+//             slideShadows: false,
+//         },
+//         breakpoints: {
+//             767.98: {
+//                 spaceBetween: 0
+//             }
+//         }
+//     })
+// });
