061752e9f6f1ef40baa415a5ca50fda95506de1a
Billy <contact@akatek.vn>
2023-02-15 10:43:36 +0700

    initial commit

--- js/index.js
+++ js/index.js
@@ -30,11 +30,51 @@
 //     })
 // });
 
-const navItems = document.querySelectorAll(".header_nav-list_item")
-const headerNav = document.querySelector("#headerMenu")
-navItems.forEach(e=>{
-    e.addEventListener("click",e=>{
-        headerNav.classList.remove("show")
-        document.querySelector("html").classList.remove("fixed")
+function disableFixed () {
+    document.querySelector("html").classList.remove("fixed")
+    document.querySelector("header").classList.remove("opened")
+}
+
+function handleRotateButton() {
+    let btn = document.querySelector(".header_trigger")
+    let icon = btn.querySelector(".icon")
+    icon.style.transform = 'rotate(130deg)'
+    btn.ariaExpanded = btn.ariaExpanded === "true"?"false":"true" 
+}
+
+function handleMenu() {
+    const navItems = document.querySelectorAll(".header_nav-list_item")
+    const headerNav = document.querySelector("#headerMenu")
+
+    let btn = document.querySelector(".header_trigger")
+
+
+    if(document.querySelector("header").classList.contains("opened")) {
+        document.querySelector("header").classList.remove("opened")
+    }
+
+    btn.addEventListener("click", e => {
+        let icon = btn.querySelector(".icon")
+        if (btn.ariaExpanded == "false") {
+            icon.style.transform = 'rotate(130deg)'
+        } else icon.style.transform = 'rotate(220deg)'
     })
-});
+
+    document.querySelector(".header_nav-list_btn").addEventListener("click", e=> {
+        headerNav.classList.remove("show");
+        document.querySelector("header").classList.remove("opened")
+        disableFixed()
+        handleRotateButton()
+    })
+
+    navItems.forEach(e => {
+        e.addEventListener("click", e => {
+            headerNav.classList.remove("show");
+            document.querySelector("header").classList.remove("opened")
+            disableFixed()
+            handleRotateButton()
+        })
+    });
+}
+
+handleMenu()
